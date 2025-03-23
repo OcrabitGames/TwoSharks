cb578053e4b972ae72d261724a1fe7275fc6f354
Ocramaru <marcocassar@mdcusa.net>
2025-03-22 21:03:13 -0500

    Currently working on Spawner System
    - Committing this early so I can work on it on my desktop.

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] spawnMarkers;
+    private Transform[] _blueMarkers;
+    private Transform[] _pinkMarkers;
+    
+    public GameObject[] bluePrefabs;
+    public GameObject[] pinkPrefabs;
+
+    public float minSpawnTime;
+    public float maxSpawnTime;
+    
+    private float[] _blueTimes;
+    private float[] _pinkTimes;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (spawnMarkers[0] != null && spawnMarkers[1] != null)
+        {
+            _blueMarkers = new Transform[] { spawnMarkers[0].transform, spawnMarkers[1].transform };
+        }
+
+        if (spawnMarkers[2] != null && spawnMarkers[3] != null)
+        {
+            _pinkMarkers = new Transform[] { spawnMarkers[2].transform, spawnMarkers[3].transform }; 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Blue Timers
+        CheckTimers(_blueTimes,_blueMarkers, bluePrefabs);
+        
+        // Pink Side
+        CheckTimers(_pinkTimes,_pinkMarkers, pinkPrefabs);
+    }
+
+    private void CheckTimers(float[] times, Transform[] markers, GameObject[] prefabs)
+    {
+        for (int i = 0; i < times.Length; i++)
+        {
+            if (times[i] <= 0)
+            {
+                Instantiate(ChooseRandomPrefab(prefabs), ChooseRandomSpawnPosition(markers), Quaternion.identity);
+                times[i] = 0;
+            }
+        }
+    }
+    private float CreateTimeTillNextSpawn()
+    {
+        return Random.Range(minSpawnTime, maxSpawnTime);
+    }
+
+    private static GameObject ChooseRandomPrefab(GameObject[] prefabs)
+    {
+        return prefabs[Random.Range(0, prefabs.Length)];
+    }
+
+    private static Vector3 ChooseRandomSpawnPosition(Transform[] markers)
+    {
+        return markers[Random.Range(0, markers.Length)].position;
+    }
+}
